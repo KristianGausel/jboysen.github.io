27db0d74d04a06645ddc971dffa6c7ad02cff76a
Jakob Boysen <jakob@jboysen.dk>
2013-06-05 02:05:31 +0200

    added dir listing

---
+++ create_dir_list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# create directory listing for notes
+
+for DIR in $(find ./res/noter -type d); do
+  (
+    echo "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
+    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+    echo "</pre>\n</body>\n</html>"
+  ) > "${DIR}/index.html"
+done
