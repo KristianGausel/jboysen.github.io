60f2b6f39771dbebe617cf9e3e0f258079861522
Jakob Boysen <jakob@jboysen.dk>
2013-09-23 09:34:54 +0200

    google analytics

--- create_dir_list.sh
+++ create_dir_list.sh
@@ -6,18 +6,14 @@ for DIR in $(find ./cs/noter -type d); do
   (
     echo "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
-    echo "</pre>\n<script type=\"text/javascript\">
+    echo "</pre>\n<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-25480624-1']);
-_gaq.push(['_setDomainName', 'jboysen.dk']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+  ga('create', 'UA-25480624-3', 'jboysen.dk');
+  ga('send', 'pageview');
 
 </script></body>\n</html>"
   ) > "${DIR}/index.html"
